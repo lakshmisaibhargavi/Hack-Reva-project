b17603eea3757a08d8bfa928f8f8a76793539c96
Naga Nithin <56812010+naganithin@users.noreply.github.com>
2020-11-05 09:13:22 +0530

    Update Blockchain.sol

--- Blockchain.sol
+++ Blockchain.sol
@@ -12,28 +12,38 @@ contract reva{
         uint256 date;
         uint256 month;
         uint256 year;
-        int256 imei;
+        uint256 imei;
     }
     
     mapping (uint => details) individualFile;
     
-    function individual(string memory PName, uint256 PAge,string memory Plane,string memory Pcity,string memory Pstate,int256 Ppincode, uint Pdate, uint Pmonth, uint Pyear, int256 Pimei) public{
+    function individual(string memory Name, uint256 Age,string memory Lane,string memory City,string memory State,int256 Pincode, uint Date, uint Month, uint Year, uint256 Imei) public{
         details memory Person;
-        Person.name=PName;
-        Person.age=PAge;
-        Person.lane = Plane;
-        Person.city=Pcity;
-        Person.state=Pstate;
-        Person.pincode=Ppincode;
-        Person.date=Pdate;
-        Person.month=Pmonth;
-        Person.year=Pyear;
-        Person.imei=Pimei;
-        individualFile[Pimei]=Person;
+        Person.name=Name;
+        Person.age=Age;
+        Person.lane = Lane;
+        Person.city=City;
+        Person.state=State;
+        Person.pincode=Pincode;
+        Person.date=Date;
+        Person.month=Month;
+        Person.year=Year;
+        Person.imei=Imei;
+        individualFile[Imei]=Person;
     }
     
-    function PersonDetails(string PAge) public view returns (string memory PersonName, uint256 PersonAge,string memory Personlane,string memory Personcity,string memory Personstate,int256 Personpincode, uint Persondate, uint Personmonth, uint Personyear ){
-        details memory Person = 
+    function PersonDetails(uint256 Imei) public view returns (string memory PersonName, uint256 PersonAge,string memory Personlane,string memory Personcity,string memory Personstate,int256 Personpincode, uint Persondate, uint Personmonth, uint Personyear ){
+        details memory Person = individualFile[Imei];
+        PersonName=Person.name;
+        PersonAge=Person.age;
+        Personlane=Person.lane;
+        Personcity=Person.city;
+        Personstate=Person.state;
+        Personpincode=Person.pincode;
+        Persondate=Person.date;
+        Personmonth=Person.month;
+        Personyear=Person.year;
+        return(PersonName,PersonAge,Personlane,Personcity,Personstate,Personpincode,Persondate,Personmonth,Personyear);
     }
      
 }
